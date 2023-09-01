098db06eb022597c985c7ceca343a21028f64965
JackScottTaylor <143807424+JackScottTaylor@users.noreply.github.com>
2023-09-01 15:12:57 +0100

    Update analysis_tools.py

--- ECAnalyse/analysis_tools.py
+++ ECAnalyse/analysis_tools.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+def moving_average(a, n=3):
+	# Returns the moving average over n points for the provided
+	# array, a and returns a numpy array
+	# Taken from 'https://stackoverflow.com/questions/14313510/how-to-calculate-rolling-moving-average-using-python-numpy-scipy
+	ret = np.cumsum(a, dtype=float)
+	ret[n:] = ret[n:] - ret[:-n]
+	return ret[n - 1:] / n
+
