6841346d18574d99f129313e0cb3849f502a7d97
JackScottTaylor <143807424+JackScottTaylor@users.noreply.github.com>
2023-09-01 13:34:47 +0100

    Update __init__.py

--- ECAnalyse/SuperCap/__init__.py
+++ ECAnalyse/SuperCap/__init__.py
@@ -2,4 +2,4 @@ from ..analysis_tools import *
 from ..file_reader import *
 from ..plotting_tools import *
 
-print('SuperCapacitor Analysis Module Added')
+print('SuperCapacitor Analysis Module Loaded')
