e30c0af048e8b6fe2b4edf74539214f736dae8e6
JackScottTaylor <143807424+JackScottTaylor@users.noreply.github.com>
2023-09-04 12:29:36 +0100

    Bad Stuff

--- ECAnalyse/RFB/__init_.py
+++ ECAnalyse/RFB/__init_.py
@@ -3,3 +3,4 @@ from ..file_reader import *
 from ..plotting_tools import *
 
 print('Redox Flow Battery Analysis Module Loaded')
+print(np.arange(0, 5, 1))
--- testing_area.py
+++ testing_area.py
@@ -1,4 +1,1 @@
-from ECAnalyse.analysis_tools import *
-
-a = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(moving_average(a, n=12))
+from ECAnalyse.RFB import *
